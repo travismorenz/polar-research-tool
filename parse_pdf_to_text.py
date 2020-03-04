0552cdb8f549859a8404d9813ae5e7fe715944e5
Travis Morenz <travismorenz@gmail.com>
2020-03-03 22:03:58 -0800

    Include error handling

--- parse_pdf_to_text.py
+++ parse_pdf_to_text.py
@@ -9,6 +9,7 @@ from pdfminer.pdfparser import PDFParser
 
 # Based on https://pdfminersix.readthedocs.io/en/latest/tutorials/composable.html
 def parse_pdf_to_text(url):
+  try:
     pdf = urllib.request.urlopen(url).read()
     in_file = BytesIO(pdf)
     output_string = StringIO()
@@ -20,3 +21,5 @@ def parse_pdf_to_text(url):
     for page in PDFPage.create_pages(doc):
         interpreter.process_page(page)
     return output_string.getvalue()
+  except Exception as err:
+    return err

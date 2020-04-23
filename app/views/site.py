36278a42dd6a289d73e15d0a71bb06a736ceb02d
Travis Morenz <travismorenz@gmail.com>
2020-04-23 15:45:36 -0700

    Fix pagination

--- app/views/site.py
+++ app/views/site.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from app.models import db, Article
 
 site = Blueprint('site', __name__)
 
 @site.route("/", methods=['GET'])
 def intmain():
-    articles = Article.query.order_by(Article.publish_date.desc()).paginate(1, 10, error_out=False)
+    articles = Article.query.order_by(Article.publish_date.desc()).paginate(max_per_page=10, error_out=False)
     return render_template('main.html', articles=articles, tab='articles')
 
 @site.route('/library', methods=['GET'])

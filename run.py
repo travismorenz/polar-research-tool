b4f328ee6dc95e99d7a9f442ea63297c24a5cfbb
Travis Morenz <travismorenz@gmail.com>
2020-04-24 22:44:04 -0700

    Run.py now uses port specified in .env

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+import os
 from app import create_app
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    app.run(debug=True, port=int(os.getenv('APP_PORT')))

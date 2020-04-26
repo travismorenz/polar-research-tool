a7cb97495c33167e6702441863b97931ac4a800d
Travis Morenz <travismorenz@gmail.com>
2020-04-26 16:35:11 -0700

    Improved query

--- app/views/site.py
+++ app/views/site.py
@@ -8,11 +8,14 @@ def intmain():
     articles = Article.query
     if session.get('selected-project') and session['selected-project'] != "none":
         project = Project.query.filter_by(name=session['selected-project']).first()
-        articles = db.session.query(Article).distinct()\
-            .join(articles_categories)\
-            .join(articles_keyphrases)\
-            .join(projects_categories, (projects_categories.c.category_id == articles_categories.c.category_id) & (projects_categories.c.project_id == project.id))\
-            .join(projects_keyphrases, (projects_keyphrases.c.keyphrase_id == articles_keyphrases.c.keyphrase_id) & (projects_keyphrases.c.project_id == project.id))
+        # Query all articles that share at least one keyphrase and category with the project
+        cat_sq = db.session.query(articles_categories)\
+            .join(projects_categories, (articles_categories.c.category_id == projects_categories.c.category_id) & (projects_categories.c.project_id == project.id))\
+            .filter(articles_categories.c.article_id == Article.id)
+        kp_sq = db.session.query(articles_keyphrases)\
+            .join(projects_keyphrases, (articles_keyphrases.c.keyphrase_id == projects_keyphrases.c.keyphrase_id) & (projects_keyphrases.c.project_id == project.id))\
+            .filter(articles_keyphrases.c.article_id == Article.id)
+        articles = db.session.query(Article).filter(cat_sq.exists() & kp_sq.exists())
     articles = articles.order_by(Article.publish_date.desc())
     articles = articles.paginate(max_per_page=10, error_out=False)
 

a600d6b7189eeb94eea859899dcb33b84dbccc84
Travis Morenz <travismorenz@gmail.com>
2020-07-10 16:08:54 -0700

    Commenting and reorganizing

--- app/views/site.py
+++ app/views/site.py
@@ -81,7 +81,9 @@ def params_reduce_helper(a, b):
 
 @site.route("/", methods=['GET'])
 def intmain():
+    # Query params
     page = int(request.args.get('page')) if request.args.get('page') else 0
+    search_string = request.args.get('search')
 
     # Filter articles if project is selected
     filter_query = ""
@@ -91,13 +93,12 @@ def intmain():
         filter_query = get_filter_query(project.id)
 
     # Filter articles on search string
-    search_string = ""
     search_query = ""
-    search_params = None
+    search_query_params = None
     if search_string:
         terms = list(filter(lambda x: x, search_string.split(' '))) # split search string on space, remove empty strings
         search_query = build_search_query(project, terms)
-        search_params = reduce(params_reduce_helper, terms, {}) # convert to dict of sql params
+        search_query_params = reduce(params_reduce_helper, terms, {}) # convert to dict of sql params
 
     # Construct queries
     main_query = f"""
@@ -115,18 +116,20 @@ def intmain():
         {filter_query}
         {search_query};
     """
-    # Populate articles dict with query results and metadata
+
+    # Populate articles with our query results
     articles = {}
     query_params = {'limit': LIMIT, 'offset': page * LIMIT}
     if project:
         query_params['id'] = project.id
-    if search_params:
-        query_params.update(search_params)
+    if search_query_params:
+        query_params.update(search_query_params)
     articles['items'] = Article.query.from_statement(db.text(main_query)).params(**query_params).all()
     articles['total'] = db.engine.execute(db.text(count_query), **query_params).fetchall()[0]['count']
+
+    # Add additional metadata to articles for display
     set_pagination_info(articles, page)
     set_previous_versions(articles)
-
     return render_template('main.html', articles=articles, tab='articles', project=project)
 
 
@@ -137,9 +140,12 @@ def library():
     page = int(request.args.get('page')) if request.args.get('page') else 0
     project = Project.query.filter_by(name=session["selected-project"]).first()
 
+    # Populate articles with our query results
     articles = {}
     articles['items'] = project.articles.order_by(Article.publish_date.desc()).limit(LIMIT).offset(page * LIMIT).all()
     articles['total'] = project.articles.count()
+
+    # Add additional metadata to articles for display
     set_pagination_info(articles, page)
     set_previous_versions(articles)
     return render_template('main.html', articles=articles, tab='library', project=project)
